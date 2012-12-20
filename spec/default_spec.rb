fcfbb34eda936b3cfa7f768042edfb1c6a131ff0
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-12-20 14:47:21 +0100

    Sort config entries so the file content stays the same

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -7,31 +7,23 @@ describe 'The recipe git-buildpackage::default' do
     chef_run.should install_package 'git-buildpackage'
   end
 
-  # content test needs ordered hash
-  if RUBY_VERSION >= '1.9'
-    it 'should create the git-buildpackage config file with right content' do
-      content = <<'EOF'
+  it 'should create the config file with the right content' do
+    content = <<'EOF'
 [DEFAULT]
 builder = debuild -i\.git -I.git -uc -us
 cleaner = debuild clean
-upstream-branch = upstream
-upstream-tag = upstream/%(version)s
 debian-branch = master
 debian-tag = debian/%(version)s
 ignore-branch = False
+upstream-branch = upstream
+upstream-tag = upstream/%(version)s
 
 [git-buildpackage]
-prebuild = 
 postbuild = 
 posttag = 
+prebuild = 
 EOF
-      chef_run.should create_file_with_content \
-        chef_run.node['git-buildpackage']['config_file'], content
-    end
-  else
-    it 'should create the git-buildpackage config file' do
-      chef_run.should create_file \
-        chef_run.node['git-buildpackage']['config_file']
-    end
+    chef_run.should create_file_with_content \
+      chef_run.node['git-buildpackage']['config_file'], content
   end
 end
