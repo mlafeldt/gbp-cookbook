46becf60fd5fa753083dc21beb1acc99fed0d8fd
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-07-29 13:55:17 +0200

    1.0.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mathias.lafeldt@gmail.com"
 license           "Apache 2.0"
 description       "Installs and configures git-buildpackage"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.4"
+version           "1.0.5"
 recipe            "git-buildpackage::default", "Installs and configures git-buildpackage"
 
 supports "ubuntu"
